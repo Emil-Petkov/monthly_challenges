e9a59d315d65d5a0fef4a5be2cc1a718aecfb953
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-12-18 15:57:45 +0200

    fix: resolve redirect URL paths to avoid hardcoding

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 
 monthly_chalenges = {
@@ -25,8 +26,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Month number is out of range.")
 
     redirect_month = months[month - 1]
-
-    return HttpResponseRedirect('/challenges/' + redirect_month)
+    redirect_path = reverse('month_challenge', args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
